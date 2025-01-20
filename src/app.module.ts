07296222c17c67022b86c70f9bd3928de7ca2f5b
3405000 <jaeeunnooh@gmail.com>
2025-01-20 16:25:27 +0900

    feat: 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardModule } from './board/board.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BoardModule, AuthModule],
 })
 export class AppModule {}
+
+// @Module({
+//   imports: [],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}

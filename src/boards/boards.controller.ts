97b3ec49050c2668e45e84e8c34aff98ac529e57
3405000 <jaeeunnooh@gmail.com>
2025-01-21 12:36:24 +0900

    feat: 게시글 조회 기능 구현 (controller 및 service)

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './boards.entity';
 
 @Controller('boards')
 export class BoardsController {
@@ -7,8 +8,9 @@ export class BoardsController {
     constructor(private boardsService: BoardsService) { }
     
     // 주입된 인스턴스는 메서드 내에서 사용
-    @Get('hello')
-    async getHello(): Promise<string> {
-        return this.boardsService.hello();
+    // 게시글 조회 기능
+    @Get('/')
+    getAllBoards(): Board[] {
+        return this.boardsService.getAllBoards();
     }
 }

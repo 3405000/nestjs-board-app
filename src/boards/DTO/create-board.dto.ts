fb11070aaf514a0e61191c1cf2f40f376f4ad51b
3405000 <jaeeunnooh@gmail.com>
2025-01-22 11:38:54 +0900

    refactor: 게시글 작성 기능에 유효성검사 파이프 추가

--- src/boards/DTO/create-board.dto.ts
+++ src/boards/DTO/create-board.dto.ts
@@ -1,5 +1,13 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class CreateBoardDTO {
+    @IsNotEmpty()
+    @IsString()
     author: string;
+
+    @IsNotEmpty()
     title: string;
+
+    @IsNotEmpty()
     contents: string;
 }

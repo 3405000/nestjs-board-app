8bad6f92ae1ba1aa074da333dae7dbbf98f14049
3405000 <jaeeunnooh@gmail.com>
2025-02-04 11:27:05 +0900

    feat: 회원가입 시 비밀번호 암호화(BCrypt 해싱) 적용

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDTO } from './DTO/create-user.dto';
 import { UserRole } from './users-role.enum';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -16,7 +17,8 @@ export class AuthService {
             throw new BadRequestException('Something went wrong')
         }
         await this.checkEmailExist(email)
-
+        const hashedPassword = await this.hashPassword(password)
+        
         const user: User = {
             id: 0,
             username,
@@ -32,5 +34,10 @@ export class AuthService {
         if (existingUser) {
             throw new ConflictException('Email already exists')
         }
+    }
+
+    async hashPassword(password: string): Promise<string> {
+        const salt = await bcrypt.genSalt() // 솔트 : 비밀번호 해싱할 때 보안성을 높이기 위해 추가되는 난수
+        return await bcrypt.hash(password, salt) // 비밀번호 해싱
     }
 }

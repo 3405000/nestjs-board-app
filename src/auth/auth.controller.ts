87cc4f32ced4ba1980e36271992bf5d68c9e0b16
3405000 <jaeeunnooh@gmail.com>
2025-02-04 10:54:26 +0900

    feat: 회원가입 기능 기초 구조 생성

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDTO } from './DTO/create-user.dto';
+import { UserResponseDTO } from './DTO/user-response.dto';
 
-@Controller('auth')
+@Controller('api/auth')
 export class AuthController {
     constructor(private authService: AuthService){}
-    
+
+    // 회원 가입 기능
+    @Post('/signup')
+    async createUser(@Body() createUserDTO: CreateUserDTO): Promise<UserResponseDTO> {
+        return new UserResponseDTO(await this.authService.createUser(createUserDTO))
+    }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { User } from './users.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDTO } from './DTO/create-user.dto';
+import { UserRole } from './users-role.enum';
 
 @Injectable()
 export class AuthService {
     constructor(@InjectRepository(User) private userRepository: Repository<User>){}
+
+    // 회원가입 기능
+    async createUser(createUserDTO: CreateUserDTO): Promise<User> {
+        const { username, password, email, role } = createUserDTO
+        if (!username || !password || !email || !role) {
+            throw new BadRequestException('Something went wrong')
+        }
+
+        const user: User = {
+            id: 0,
+            username,
+            password,
+            email,
+            role: UserRole.USER
+        }
+        return user
+    }
 }

64b4d4c562183318966c6a54b6339c717a356ddd
3405000 <jaeeunnooh@gmail.com>
2025-02-06 12:47:46 +0900

    feat: Interceptor, Filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
-import { Module } from '@nestjs/common';
-import { BoardsModule } from './boards/boards.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeORMConfig } from './config/typeorm.config';
-import { AuthModule } from './auth/auth.module';
-import { GlobalModule } from 'global.module';
+import { Module } from '@nestjs/common'
+import { BoardsModule } from './boards/boards.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { typeORMConfig } from './config/typeorm.config'
+import { AuthModule } from './auth/auth.module'
+import { GlobalModule } from 'global.module'
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core'
+import { UnauthorizedExceptionFilter } from './common/filters/unauthorization.filter'
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor'
 
 @Module({
   imports: [
@@ -12,6 +15,16 @@ import { GlobalModule } from 'global.module';
     BoardsModule,
     AuthModule,
   ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: UnauthorizedExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ]
 })
 export class AppModule {}
 
